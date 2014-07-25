145943494d3f7e15d2a96aa164747056478df2df
Ildar Kayumov <ildar.kayumov@flatstack.com>
2014-07-25 17:58:30 +0400

    Add show article action

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'dashboard#index'
 
-  resources :articles, only: %w(new create)
+  resources :articles, only: %w(new create show)
 end
